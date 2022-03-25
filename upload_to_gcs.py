7c3aece1ce8d26dfe12bb0d80afc5eacb79b20a1
pourquoi <mathias.dusautoy@gmail.com>
2022-03-25 14:07:33 +0100

    typo

--- upload_to_gcs.py
+++ upload_to_gcs.py
@@ -5,7 +5,7 @@ from google.cloud import storage
 GOOGLE_CREDENTIALS_PATH = 'key.preprod.json'
 BUCKET = 'terraform-helpyourshelf-test-private-uploads-92ui8d'
 
-client = storage.Client.from_service_account_json(json_credentials_path='test.json')
+client = storage.Client.from_service_account_json(json_credentials_path=GOOGLE_CREDENTIALS_PATH)
 bucket = client.get_bucket(BUCKET)
 
 object_name_in_gcs_bucket = bucket.blob('video-test-' + (''.join(random.choice(string.digits) for i in range(10))) + '.mp4')
